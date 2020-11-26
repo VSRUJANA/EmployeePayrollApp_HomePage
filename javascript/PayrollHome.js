ee26b2e64382f347e0c00ab656cb61c3652d2cc7
VSRUJANA <srujana.valavala@gmail.com>
2020-11-26 21:51:54 +0530

    [srujana] Add. View EmployeeDetails in tabular format from JSON object

--- javascript/PayrollHome.js
+++ javascript/PayrollHome.js
@@ -1,34 +1,118 @@
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', ()=>
+{
     createEmployeeTable();
 });
-
-// Template literal ES6 feature
-const createEmployeeTable = ()=>{
-    const innerHtml = `
-    <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Department</th>
-        <th>Salary</th>
-        <th>Start Date</th>
-        <th>Actions</th>
-    </tr>
-    <tr>
-        <td><img src="../assets/ProfilePics/Ellipse -7.png" alt=""></td>
-        <td>Srujana Valavala</td>
-        <td>Female</td>
-        <td>
-            <div class="dept-label">Finance</div>
-            <div class="dept-label">HR</div>
-        </td>
-        <td>&#x20B9 4,50,000</td>
-        <td>18 Sep 2020</td>
-        <td>
-            <img class="action-label" id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete.svg">
-            <img class="action-label" id="2" onclick="update(this)" alt="update" src="../assets/icons/create.svg">
-        </td>
-    </tr>
-    `;
-    document.querySelector(".employeeTable").innerHTML = innerHtml;
+const createEmployeeTable = () =>
+{
+    let empPayrollList = createJsonFile();
+    const headerHTML = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
+    let innerHtml = `${headerHTML}`;
+    for(const empPayrollData of empPayrollList)
+    {
+        innerHtml = `${innerHtml}  
+         <tr>
+            <td><img alt="profilePic" src="${empPayrollData._profilePic}"></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>
+            ${getDeptHtml(empPayrollData._department)}
+            </td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+            <img class="action-label" id="${empPayrollData.id}" onclick="remove(this)" src="../assets/icons/delete.svg">
+            <img class="action-label" id="${empPayrollData.id}" onclick="update(this)" src="../assets/icons/create.svg">
+            </td>
+        </tr>
+        `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
 } 
+
+const getDeptHtml = (deptList) => 
+{
+    let deptHtml = '';
+    for (const dept of deptList) 
+    {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createJsonFile = () =>
+{
+    let empPayrollList = [
+        
+        {
+            _name:'Srujana Valavala',
+            _gender:'Female',
+            _department:['Finance','Sales'],
+            _salary:'350000',
+            _startDate:'18 Sep 2020',
+            _note:'',
+            _profilePic:'../assets/ProfilePics/Ellipse -7.png',
+            _id:new Date().getTime()
+        },
+        {
+            _name:'Devrath Dixit',
+            _gender:'Male',
+            _department:['HR','Sales','Finance'],
+            _salary:'500000',
+            _startDate:'24 Oct 2019',
+            _note:'',
+            _profilePic:'../assets/ProfilePics/Ellipse -5.png',
+            _id:new Date().getTime()
+        },
+        {
+            _name:'Sumanjali Gidda',
+            _gender:'Female',
+            _department:['Sales'],
+            _salary:'450000',
+            _startDate:'28 Jan 2018',
+            _note:'',
+            _profilePic:'../assets/ProfilePics/Ellipse -1.png',
+            _id:new Date().getTime()
+        },
+        {
+            _name:'Tony Stark',
+            _gender:'Male',
+            _department:['Engineer','Analyst','HR'],
+            _salary:'250000',
+            _startDate:'12 Jul 2019',
+            _note:'',
+            _profilePic:'../assets/ProfilePics/Ellipse -2.png',
+            _id:new Date().getTime()
+        },
+        {
+            _name:'Steve Rogers',
+            _gender:'Male',
+            _department:['Logistics'],
+            _salary:'350000',
+            _startDate:'16 Dec 2017',
+            _note:'',
+            _profilePic:'../assets/ProfilePics/Ellipse -8.png',
+            _id:new Date().getTime()
+        },
+        {
+            _name:'Sowjanya Buddaraju',
+            _gender:'Female',
+            _department:['HR','Analyst'],
+            _salary:'350000',
+            _startDate:'22 May 2019',
+            _note:'',
+            _profilePic:'../assets/ProfilePics/Ellipse -4.png',
+            _id:new Date().getTime()
+        },
+        {
+            _name:'Ravi Yeluri',
+            _gender:'Female',
+            _department:['HR'],
+            _salary:'500000',
+            _startDate:'18 Feb 2016',
+            _note:'',
+            _profilePic:'../assets/ProfilePics/Ellipse -3.png',
+            _id:new Date().getTime()
+        }
+    ]
+    return empPayrollList;
+}
